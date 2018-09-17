77531454fbb3884af58bdd4416658138c8d34725
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-09-17 11:21:08 -0700

    added appsync

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from 'aws-amplify-react'
+import { API, graphqlOperation } from 'aws-amplify'
+
+const ListTodos = `
+  query {
+    listTodos {
+      items {
+        id name description completed
+      }
+    }
+  }
+`
 
 class App extends Component {
+  state = { todos: [] }
+  async componentDidMount() {
+    const todoData = await API.graphql(graphqlOperation(ListTodos))
+    this.setState({ todos: todoData.data.listTodos.items })
+  }
   render() {
     return (
       <div className="App">
@@ -12,9 +28,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.todos.map((todo, i) => (
+            <div>
+              <h3>{todo.name}</h3>
+              <p>{todo.description}</p>
+            </div>
+          ))
+        }
       </div>
     );
   }
