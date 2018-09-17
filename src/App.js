be911eb7af4b0a56098d861dc9ebb1f265e352de
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-09-17 12:19:51 -0700

    updated rest api code

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from 'aws-amplify-react'
+import { API } from 'aws-amplify'
 
 class App extends Component {
+  state = { people: [] }
+  async componentDidMount() {
+    const data = await API.get('peopleapi', '/people')
+    this.setState({ people: data.people })
+  }
   render() {
     return (
       <div className="App">
@@ -12,9 +18,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.people.map((person, i) => (
+            <div>
+              <h3>{ person.name }</h3>
+              <p>{ person.hair_color }</p>
+            </div>
+          ))
+        }
       </div>
     );
   }
