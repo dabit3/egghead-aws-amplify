da99907bca3c17176938503d41be57bf753f9133
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-09-17 14:44:48 -0700

    storage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from 'aws-amplify-react'
+import { Storage } from 'aws-amplify'
 
 class App extends Component {
+  state = { file: '', filename: '' }
+  componentDidMount() {
+    Storage.list('')
+      .then(d => {
+        console.log('s3 data: ', d)
+      })
+      .catch(err => {
+        console.log('error: ', err)
+      })
+  }
+  handleChange = (e) => {
+    const file = e.target.files[0];
+    console.log('file: ', file)
+    this.setState({
+      fileName: file.name,
+      file: URL.createObjectURL(file)
+    })
+  }
+  saveFile = () => {
+    Storage.put(this.state.filename, this.state.file, {
+      contentType: 'image/png'
+    })
+    .then (result => {
+      console.log(result)
+      alert('image successfully uploaded!')
+      this.setState({ file: '' })
+    })
+    .catch(err => console.log(err));
+  }
   render() {
     return (
       <div className="App">
@@ -15,6 +45,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="file" onChange={this.handleChange}/>
+        <img src={this.state.file}/>
+        <button onClick={this.saveFile}>Save File</button>
       </div>
     );
   }
